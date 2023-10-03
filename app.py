5dcc89548775f7e26f8a45a7aac0fb8742ba1663
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 21:55:13 +0300

    added data to query if user password match

--- app.py
+++ app.py
@@ -62,7 +62,16 @@ def home():
 @app.route('/login' , methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password,form.password.data):
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
+
+@app.routes('/dashboard',methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
 
 @app.route('/register', methods=['POST'])
 def register():
