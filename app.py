beab1a548d1db64fa6a789cfd7419567ca96dca6
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 21:44:07 +0300

    added login

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager,login_required,logout_user,current_user
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, ValidationError
 from flask_wtf import FlaskForm
+from flask_bcrypt import Bcrypt
 
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
 app.config['SECRET_KEY'] = 'kipsiele98'
 
-#login_manager = LoginManager()
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view="login"
+
+@login_manager.user_loader
+def login_user(user_id):
+    return User.query.get(int(user_id))
 
 
-db = SQLAlchemy(app)
+
 class User(db.model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30),nullable=False, unique=True)
@@ -55,11 +64,23 @@ def login():
     form = LoginForm()
     return render_template('login.html', form=form)
 
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register', methods=['POST'])
 def register():
     form = RegistrationForm()
+
+
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data,password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
     return render_template('register.html', form=form)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
