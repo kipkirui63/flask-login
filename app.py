6c69d15ea1512af7e76a2841901ae30813f93d42
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 11:54:50 +0300

    created files

---
+++ app.py
@@ -0,0 +1,13 @@
+# from flask import Flask , jsonify
+
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     data = jsonify({"message": "welcome to flask"})
+#     return data
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
