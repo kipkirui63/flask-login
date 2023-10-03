b59c2147508c96c3966ddde524656e9e50386598
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 14:31:16 +0300

    added database

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask , jsonify, render_template ,url_for
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
 
 @app.route('/')
 def home():
