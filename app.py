3e3d112a09fe854f31e677ddc4b0b68ac2874b4b
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 15:25:04 +0300

    added user models

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask , jsonify, render_template ,url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
+app.config['SECRET_KEY'] = 'kipsiele98'
+
+class User(db.model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String,nullable=False)
+    password = db.Column(db.String,nullable=False)
+
+    
+
+
 
 @app.route('/')
 def home():
