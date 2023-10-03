756fd37ddc3e0c2048cc05c939c7c45c48a8d87c
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 20:39:20 +0300

    added login and registration form

--- app.py
+++ app.py
@@ -1,35 +1,64 @@
-from flask import Flask , jsonify, render_template ,url_for
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, ValidationError
+from flask_wtf import FlaskForm
+
 
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
 app.config['SECRET_KEY'] = 'kipsiele98'
 
+#login_manager = LoginManager()
+
+
+db = SQLAlchemy(app)
 class User(db.model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String,nullable=False)
-    password = db.Column(db.String,nullable=False)
+    username = db.Column(db.String(30),nullable=False, unique=True)
+    password = db.Column(db.String(40),nullable=False)
 
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder":"username"})
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder":"password"})
+    submit = SubmitField('Register')
     
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError;{"That username already exists"}
 
 
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder":"username"})
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=4, max=20)], render_kw={"placeholder":"password"})
+    submit = SubmitField('Login')
+    
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError;{"That username already exists"}
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/login')
+@app.route('/login' , methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
     return render_template('login.html')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    form = RegistrationForm()
     return render_template('register.html')
 
 if __name__ == '__main__':
