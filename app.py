bc2a7e6451ee623d2de21b776f9cae1cb05228a0
kipkirui63 <enocksang8356@gmail.com>
2023-10-03 20:54:58 +0300

    added input form

--- app.py
+++ app.py
@@ -53,13 +53,13 @@ def home():
 @app.route('/login' , methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template('login.html')
+    return render_template('login.html', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
-    return render_template('register.html')
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
